858cff3de57a6af044ff450c14aa4700f3fe2d81
applewang <nearby.ren@gmial.com>
2022-08-13 21:41:19 +0800

    ...

---
+++ libs-module/src/main/java/ejiayou/libs/module/T.kt
@@ -0,0 +1,14 @@
+package ejiayou.libs.module
+
+import android.util.Log
+
+/**
+ * @author: lr
+ * @created on: 2022/8/13 9:22 下午
+ * @description:
+ */
+object T {
+    fun test() {
+        Log.d("t","ttt")
+    }
+}
